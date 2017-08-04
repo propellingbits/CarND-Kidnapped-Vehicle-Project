d7fbbc07f11ffbb428f264cbea59a4cfbb00afaf
propellingbits <bullet1128@gmail.com>
2017-08-03 19:46:54 -0500

    finish rest of implementation for this project

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -13,6 +13,11 @@
 #include "particle_filter.h"
 #include "helper_functions.h"
 #include "map.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 using namespace std;
 static default_random_engine gen;
 
@@ -22,7 +27,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 	
-	num_particles = 250;	
+	num_particles = 101;	
 	// normal (Gaussian) distributions.
 	normal_distribution<double> dist_x(x, std[0]);
 	normal_distribution<double> dist_y(y, std[1]);
@@ -32,15 +37,14 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	{
 		Particle pf = Particle();
 		pf.id = i;
-		pf.x = x;
+		/*pf.x = x;
 		pf.y = y;
-		pf.theta = theta;
-		pf.weight = 1.0;
-		//adding noise
-		pf.x += dist_x(gen);
-		pf.y += dist_y(gen);
-		pf.theta += dist_theta(gen);
-
+		pf.theta = theta;*/
+		pf.weight = 1.0;		
+		pf.x = dist_x(gen);
+		pf.y = dist_y(gen);
+		pf.theta = dist_theta(gen);
+		weights.push_back(1.0);
 		particles.push_back(pf);
 	}
 
@@ -62,7 +66,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	for (int i = 0; i < num_particles; i++)
 	{
 		Particle p = particles[i];
-		if (fabs(yaw_rate) > 0.001) {
+		if (yaw_rate > 0) { //also test for fabs(yaw_rate) > 0.001
 			p.x += (velocity / yaw_rate)*(sin(p.theta + yaw_rate*delta_t) - sin(p.theta));
 			p.y += (velocity / yaw_rate)*(cos(p.theta) - cos(p.theta + yaw_rate*delta_t));
 			p.theta += yaw_rate*delta_t;
@@ -95,7 +99,7 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 		{
 			LandmarkObs pred = predicted[j];
 
-			double dist_op = dist(obs.x, obs.y, pred.x, pred.y);
+			double dist_op = dist(obs.x, obs.y, pred.x, pred.y); //dist between o(bservation) and p(redicted)
 
 			if (dist_op < nearest_ngbr_dist)
 			{
@@ -139,7 +143,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		}
 
 		vector<LandmarkObs> transformedLandmarks;
-		for (int o = 0; i < observations.size(); o++)
+		for (int o = 0; o < observations.size(); o++)
 		{
 			//converting from vehicle to map co-ordinates
 			double x = observations[o].x * cos(p.theta) - observations[o].y * sin(p.theta) + p.x;
@@ -151,14 +155,14 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
 		for (int t = 0; t < transformedLandmarks.size(); t++)
 		{
-			double p_x, p_y, m_x, m_y;
+			double p_x=0.0, p_y=0.0, m_x=0.0, m_y=0.0;
 			m_x = transformedLandmarks[t].x; //measured
 			m_y = transformedLandmarks[t].y;
 			for (int s = 0; s < sensedLandmarks.size(); s++)
 			{
 				if (sensedLandmarks[s].id == transformedLandmarks[t].id)
 				{
-					p_x = sensedLandmarks[s].x;
+					p_x = sensedLandmarks[s].x; //predicted
 					p_y = sensedLandmarks[s].y;
 					break;
 				}
@@ -167,7 +171,8 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 			double std_x = std_landmark[0];
 			double std_y = std_landmark[1];
 
-			exp(-0.5*())
+			double mwg_w = (1 / (2 * M_PI*std_x*std_y)) * exp(-(pow(p_x - m_x, 2) / (2 * pow(std_x, 2)) + (pow(p_y - m_y, 2) / (2 * pow(std_y, 2)))));
+			particles[i].weight *= mwg_w;			
 		}
 	}
 }
@@ -177,12 +182,36 @@ void ParticleFilter::resample() {
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 
+	discrete_distribution<int> distribution(weights.begin(), weights.end());
+	vector<Particle> resample_particles;
+	for (int i = 0; i < num_particles; i++)
+	{
+		resample_particles.push_back(particles[distribution(gen)]);
+	}
+
+	particles = resample_particles;
 }
 
 double * ParticleFilter::weighted_mean_error(double gt_x, double gt_y, double gt_theta) {
 	// TODO: Calculate the weighted mean error of the particle filter.
 	static double error[3];
-
+	//Particle max = *max_element(particles.begin(), particles.end()); // there is * coz it return an iterator
+	double max_weight = 0.0;
+	Particle best_particle = Particle();
+	for (int i = 0; i < num_particles; i++) {
+		if (particles[i].weight > max_weight) {
+			best_particle = particles[i];
+			max_weight = best_particle.weight;
+		}
+	}
+		
+	error[0] = (best_particle.x - gt_x);
+	error[1] = floor(best_particle.y - gt_y);
+	error[2] = (best_particle.theta - gt_theta);
+	/*error[2] = fmod(error[2], 2.0 * M_PI);
+	if (error[2] > M_PI) {
+		error[2] = 2.0 * M_PI - error[2];
+	}*/
 	return error;
 }
 
